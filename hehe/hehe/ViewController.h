0cdbd18a30bc747e5a6ada20c1a3feec9d4e5465
全日制 <apple@quanrizhideiMac-6.local>
2015-08-25 16:40:10 +0800

    haha

---
+++ hehe/hehe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hehe
+//
+//  Created by 全日制 on 15/8/25.
+//  Copyright (c) 2015年 全日制. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ hehe/hehe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  hehe
+//
+//  Created by 全日制 on 15/8/25.
+//  Copyright (c) 2015年 全日制. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
